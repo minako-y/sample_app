ff6e0a6c1cb066ecd9f0a59028ba6d67d5dc1ab5
minako-y <m.tp2uk@gmail.com>
2021-06-04 13:04:43 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '70d1eb5537adfd5ba2996dbef78d8605b82785b16a68decee5ba9ded83bc60eb6192deb54d4a39be4c95615eb74219e1208ef1ccba591e4cc24c292b9b93e60c'
