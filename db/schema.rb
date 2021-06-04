0ed9a6f3be0d174e8858aae7ba5b9ce3a46ec40f
minako-y <m.tp2uk@gmail.com>
2021-06-04 10:24:54 +0000

    [Finish]2-4

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_082954) do
+ActiveRecord::Schema.define(version: 2021_06_04_091151) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
